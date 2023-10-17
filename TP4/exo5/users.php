c90b121c6f75d915e13c0ac8bda948b9599f5681
AurelT59 <aurelien.terrades@gmail.com>
2023-10-17 12:03:17 +0200

    TP4 modif 2

---
+++ TP4/exo5/users.php
@@ -0,0 +1,52 @@
+<?php
+
+header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+
+require_once('db_connect.php');
+
+switch ($_SERVER['REQUEST_METHOD']) {
+    case 'GET':
+        $request = $pdo->prepare("SELECT * FROM users");
+        $request->execute();
+        $result = $request->fetchAll(PDO::FETCH_OBJ);
+
+        echo json_encode($result);
+        break;
+
+    case 'POST':
+
+        $data = json_decode(file_get_contents("php://input"));
+
+        $request = $pdo->prepare("INSERT INTO `users` (`id`, `name`, `email`) VALUES ('','" . $data['name'] . "','" . $data['mail'] . "')");
+        $request->execute();
+        $result = $request->fetchAll(PDO::FETCH_OBJ);
+
+        echo json_encode($result);
+        break;
+
+    case 'PUT':
+
+        $data = json_decode(file_get_contents("php://input"));
+
+        $request = $pdo->prepare("UPDATE users SET name = '" . $data['name'] . "', email = '" . $data['mail'] . "' WHERE id = " . $data['changeId']);
+        $request->execute();
+        $result = $request->fetchAll(PDO::FETCH_OBJ);
+
+        echo json_encode($result);
+        break;
+
+    case 'DELETE':
+        $data = json_decode(file_get_contents("php://input"));
+
+        $request = $pdo->prepare("DELETE FROM `users` WHERE `id` = " . $data['id']);
+        $request->execute();
+        $result = $request->fetchAll(PDO::FETCH_OBJ);
+
+        echo json_encode($result);
+        break;
+
+    default:
+        echo json_encode(array('message' => 'Méthode non autorisée'));
+        break;
+}
